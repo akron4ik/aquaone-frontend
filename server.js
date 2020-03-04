0b91dcb4ea006cee08309cdfb7be5a39a7403417
Anton Aronov <aron4ik@ro.ru>
2020-03-04 21:46:09 +0300

    add Dockerfile

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-/*
-/!*
-const path = require ('path');
-const express = require ('express');
-const app = express ();
-const publicPath = `${__dirname}/public`;
-//const publicPath = path.join (__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use (express.static (publicPath));
-app.get ('*', (req, res) => {
-    res.sendFile (path.join (publicPath, 'index.html'));
-});
-app.listen (port, () => {
-    console.log ('public', publicPath);
-    console.log ('dir name', __dirname);
-    console.log ('Сервер запущен !');
-});*!/*/
+//If we deploy only front on heroku, we need open this code
 
 
-
-const express = require('express');
-//const favicon = require('express-favicon');
+/*const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-//app.use(__dirname + '/build/favicon.ico');
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
@@ -35,4 +15,4 @@ app.get('/!*', function (req, res) {
 });
 app.listen(port, () => {
     console.log ('Сервер запущен !');
-})
+})*/
