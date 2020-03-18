228e1a7b42badcf0e9b4af6273e8c236d0295ab4
Anton Aronov <aron4ik@ro.ru>
2020-03-18 23:22:07 +0300

    fix bug with frequent sending

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 //If we deploy only front on heroku, we need open this code
 
 
-/*const express = require('express');
+const express = require('express');
+const favicon = require('serve-favicon')
 const path = require('path');
+/*const cors = require('cors');*/
 const port = process.env.PORT || 3000;
 const app = express();
+
+/*app.use(cors());*/
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
+/*app.get('/ping', function (req, res) {
     return res.send('pong');
-});
-app.get('/!*', function (req, res) {
+});*/
+/*app.get('/!*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+});*/
 app.listen(port, () => {
     console.log ('Сервер запущен !');
-})*/
+})
